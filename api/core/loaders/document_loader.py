335d06855820c381f950b5f1af8208a3aa4dd6c4
Ayush Joshi <ayush854032@gmail.com>
2025-01-11 18:20:47 +0530

    Added definition of a document loader to load a json document from a credit card document
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

---
+++ api/core/loaders/document_loader.py
@@ -0,0 +1,76 @@
+import os
+import pathlib
+import json
+import logging
+from typing import List, Dict
+
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+BASE_PROMPTS_DIR = pathlib.Path(os.getenv("BASE_PROMPTS_DIR"))
+_credit_cards_details_extractor_prompt = (
+    BASE_PROMPTS_DIR / "extraction/credit_cards_details.txt"
+)
+
+
+def llm_response_to_json(message: str) -> Dict:
+    """Structures the model response into JSON format.
+
+    This function provides a unified value for all the responses obtained from the
+    model.
+
+    Args:
+        message: The message string obtained from the JSON response.
+
+    Returns:
+        The JSON response.
+    """
+    data = {
+        "type": None,
+        "data": None,
+    }
+
+    try:
+        # Attempt to parse the JSON response
+        response = json.loads(message.replace("\n", ""))
+        data["type"] = "json"
+        data["data"] = response
+    except json.JSONDecodeError:
+        logging.warn("Parsing failed, attempting to extract JSON from a code block.")
+        try:
+            response = message.split("```json")[1].split("```")[0].strip()
+            response = json.loads(response)
+            data["type"] = "json"
+            data["data"] = response
+        except (IndexError, json.JSONDecodeError):
+            logging.exception('Cannot parse JSON string "%s"' % message)
+
+    if not data["data"]:
+        data["type"] = "text"
+        data["data"] = message
+
+    return data
+
+
+def load_docs_from_urls(urls: List[str]) -> List[Dict]:
+    """Loads a credit card JSON doc from each url."""
+    loader = WebBaseLoader(urls)
+    docs = loader.load()
+
+    json_docs = list()
+    with open(_credit_cards_details_extractor_prompt, mode="r") as extractor:
+        prompt = extractor.read()
+
+        llm = ChatOpenAI(model="gpt-4o")
+        prompt_template = PromptTemplate.from_template(prompt)
+        chain = prompt_template | llm | StrOutputParser()
+
+        for doc in docs:
+            re = chain.invoke({"document": doc.page_content})
+            json_docs.append(llm_response_to_json(re)["data"])
+    return json_docs
--- api/urls/list_all_cards.py
+++ api/urls/list_all_cards.py
@@ -1,12 +1,28 @@
-from typing import Dict
+from typing import Dict, List
 
 from api.core.scrapers.sbi import load_sbi_credit_card_urls
 from api.core.scrapers.axis import load_axis_credit_card_urls
 from api.core.scrapers.hdfc import load_hdfc_credit_card_urls
+from api.core.loaders.document_loader import load_docs_from_urls
+
+
+def _sanitize_https_urls(urls: List[str]) -> List[str]:
+    """Filters a list of URLs and returns only those that start with 'https://'.
+
+    Args:
+        urls (List[str]): A list of URLs.
+
+    Returns:
+        List[str]: A list of URLs that start with 'https://'.
+    """
+    return [url for url in urls if url.startswith("https://")]
 
 
 def list_all_cards() -> Dict:
     cards = load_sbi_credit_card_urls()
     cards.extend(load_axis_credit_card_urls())
     cards.extend(load_hdfc_credit_card_urls())
-    return {"cards": cards}
+
+    cards = _sanitize_https_urls(cards)
+    docs = load_docs_from_urls(cards)
+    return {"cards": docs}
