95b7941fc57e7c4b3056003ca5fcacc6b63ddc34
Ayush Joshi <ayush854032@gmail.com>
2025-01-12 13:05:40 +0530

    Added end-point "get-revised-credit-card" to force run the collection of information of a particular credit card
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

---
+++ api/urls/get_revised_credit_card.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from bson.objectid import ObjectId
+
+from api.request_models.models import RevisedCreditCardRequest
+from api.db import credit_cards_collection
+from api.core.loaders.document_loader import load_docs_from_urls
+
+
+def get_revised_credit_card(req: RevisedCreditCardRequest) -> Dict:
+    unique_name = req.unique_name
+    doc = credit_cards_collection.find_one({"unique_name": unique_name})
+    if doc is None:
+        return {}
+
+    docs = load_docs_from_urls([doc["source"]])
+    credit_cards_collection.update_one({"_id": ObjectId(doc["_id"])}, {"$set": docs[0]})
+    return docs[0]
--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from typing import Dict
 from fastapi import FastAPI
 
 from api.urls.list_all_cards import list_all_cards
+from api.urls.get_revised_credit_card import get_revised_credit_card
+from api.request_models.models import RevisedCreditCardRequest
 
 app = FastAPI()
 
 
-@app.get("/list-all-cards", response_model=Dict[str, list])
+@app.get("/api/list-all-cards", response_model=Dict[str, list])
 async def list_all_cards_endpoint():
     return list_all_cards()
+
+
+@app.post("/api/get-revised-credit-card", response_model=Dict[str, Dict])
+async def get_revised_credit_card_endpoint(req: RevisedCreditCardRequest):
+    return get_revised_credit_card(req)
